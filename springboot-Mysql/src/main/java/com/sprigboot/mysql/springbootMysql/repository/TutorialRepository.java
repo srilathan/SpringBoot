3a3cddb4a4bde2a30e0472f318be9f30f99e81ed
srilathan <srilathan25@gmail.com>
2021-12-22 10:12:30 +0530

    Add files via upload
    mysql integration

---
+++ springboot-Mysql/src/main/java/com/sprigboot/mysql/springbootMysql/repository/TutorialRepository.java
@@ -0,0 +1,15 @@
+package com.sprigboot.mysql.springbootMysql.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sprigboot.mysql.springbootMysql.model.Tutorial;
+
+@Repository
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+	List<Tutorial> findByPublished(boolean published);
+
+	List<Tutorial> findByTitleContaining(String title);
+}
---
+++ springboot-Mysql/src/test/java/com/sprigboot/mysql/springbootMysql/SpringbootMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprigboot.mysql.springbootMysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
